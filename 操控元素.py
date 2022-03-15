bdc02928e479d0d1766c6b26371d1527a339014c
herman-leung <52061293+herman-leung@users.noreply.github.com>
2022-03-15 20:24:42 +0800

    加注释

--- 操控元素.py
+++ 操控元素.py
@@ -11,4 +11,4 @@ wd.get('https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd=bili
 element = wd.find_element(By.ID,'kw')
 #element.send_keys('bilibili')
 
-element.clear()
+element.clear() #清空输入的值
